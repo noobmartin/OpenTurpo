df57270f2ddd3be6cdb567b1dcaeb366d80e0f79
Alexander Rajula <alexander@rajula.org>
2012-03-11 16:27:32 +0100

    Minor change.

--- openturpo/ignitionManager.cpp
+++ openturpo/ignitionManager.cpp
@@ -3,6 +3,7 @@
 ignitionManager::ignitionManager(){
   spark_charge_time_us = 4000;
   zero_start_time = 0;
+  ign_state = IGN_FIND_TRIGGER;
 }
 
 ignitionManager::~ignitionManager(){
--- openturpo/ignitionManager.h
+++ openturpo/ignitionManager.h
@@ -1,6 +1,8 @@
 #ifndef ignitionManager_h
 #define ignitionManager_h
 
+#include "common.h"
+
 class ignitionManager{
   public:
     ignitionManager();
@@ -9,6 +11,7 @@ class ignitionManager{
   private:
     unsigned long zero_start_time;
     int spark_charge_time_us;
+    IGNITION_STATE ign_state;
 };
 
 #endif
